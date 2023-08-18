7cbdad37f1c9fffc6ef3cf4f0fedd6be2362353c
ethan <hertlethan@gmail.com>
2023-08-17 22:03:42 -0400

    added API keys and URLs to fetch from, added code to retrieve coordinates from a city name

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,3 +5,26 @@ Created 2023/08/16
 Last Edited 2023/08/17
 */
 
+//API key; 2f7d29e22fb4097f360e2ea2411af483
+
+//five-day weather
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+//geocoding
+//http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+
+function getWeatherData()
+{
+    var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=2f7d29e22fb4097f360e2ea2411af483";
+
+    fetch(geocodeURL)
+        .then(function(response)
+        {
+            console.log(response.status);
+        })
+        .then(function(data)
+        {
+            console.log(data.JSON());
+        });
+}
+
