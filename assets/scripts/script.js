49e5bc44629a874d7a18ad5fd5dd7fac6083c78c
ethan <hertlethan@gmail.com>
2023-08-19 22:17:27 -0400

    added code to convert a city name to coordinates

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,29 +2,31 @@
 ethan (average-kirigiri-enjoyer)
 SCS Boot Camp Module 6 Weekly Challenge - City Weather Dashboard
 Created 2023/08/16
-Last Edited 2023/08/17
+Last Edited 2023/08/19
 */
 
-//API key; 2f7d29e22fb4097f360e2ea2411af483
+APIKey = "2fae20ca26e4a9b0f2e2e5c58c74a9be";
 
 //five-day weather
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 //geocoding
-//http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+//http://api.openweathermap.org/geo/1.0/direct?q={CITY}&limit=1&appid={API key}
+
+var searchedCity = "London";
 
 function getWeatherData()
 {
-    var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=2f7d29e22fb4097f360e2ea2411af483";
+    var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + "&limit=1&appid=" + APIKey;
 
     fetch(geocodeURL)
         .then(function(response)
         {
-            console.log(response.status);
+            return response.json();
         })
         .then(function(data)
         {
-            console.log(data.JSON());
+            var latitude = data[0].lat;
+            var longitude = data[0].lon;
         });
 }
-
