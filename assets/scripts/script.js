6240895fa1d398d0f9466783dffcc77dafd118df
ethan <hertlethan@gmail.com>
2023-08-20 14:39:09 -0400

    added code to retrieve today's weather

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@
 ethan (average-kirigiri-enjoyer)
 SCS Boot Camp Module 6 Weekly Challenge - City Weather Dashboard
 Created 2023/08/16
-Last Edited 2023/08/19
+Last Edited 2023/08/20
 */
 
 //openweathermap API key
@@ -23,23 +23,46 @@ var longitude;
 
 var searchedCity = "London";
 
+//function to get weather data from the city the user searched for and display it to the page
 async function getWeatherData()
 {
+	//defines URL to geocode city name searched by user into coordinates
 	var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + "&limit=1&appid=" + APIKey + "&units=metric";
 
+	//retrieves coordinates of city the user searched for
 	await fetch(geocodeURL)
-		.then(function(response)
-		{
-			return response.json();
-		})
-		.then(function(data)
-		{
-			latitude = data[0].lat;
-			longitude = data[0].lon;
-		});
+	.then(function(response)
+	{
+		return response.json();
+	})
+	.then(function(data)
+	{
+		latitude = data[0].lat;
+		longitude = data[0].lon;
+	});
 
+	//defines URLs to retrieve weather data from searched city coordinates
+	var todayWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&limit=1&appid=" + APIKey + "&units=metric";
 	var fiveDayWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&limit=1&appid=" + APIKey + "&units=metric";
 
+	//retrieves today's weather data from searched city coordinates
+	await fetch(todayWeatherURL)
+	.then(function(response)
+	{
+		return response.json();
+	})
+	.then(function(data)
+	{
+		console.log("TODAY'S WEATHER IN: " + searchedCity);
+		console.log("temp: " + data.main.temp + "°C");
+		console.log("humidity: " + data.main.humidity + "%");
+		console.log("weather: " + data.weather[0].main);
+		console.log("weather description: " + data.weather[0].description);
+		console.log("icon: " + data.weather[0].icon);
+		console.log("wind: " + data.wind.speed + "KM / H");
+	});
+
+	//retrieves next five days of weather data from searched city coordinates
 	await fetch(fiveDayWeatherURL)
 	.then(function(response)
 	{
@@ -47,7 +70,7 @@ async function getWeatherData()
 	})
 	.then(function(data)
 	{
-		//HEY ETHAN; THIS IS THE DATA FOR THE FIRST DAY (OF FIVE IN THE ARRAY)
+		console.log("TOMORROW'S WEATHER IN: " + searchedCity);
 		console.log("temp: " + data.list[0].main.temp + "°C");
 		console.log("humidity: " + data.list[0].main.humidity + "%");
 		console.log("weather: " + data.list[0].weather[0].main);
